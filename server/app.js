c54dd2acb8118a812ef8e22e262bfe6efa9a5d54
Kevin Defalco <kevindefalco@gmail.com>
2022-11-11 18:02:36 -0300

    deploy app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import postRoutes from "./routes/posts.routes.js";
 import  fileUpload from "express-fileupload";
-
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // middlewares- antes de que llegue a la ruta hay que procesar cosas
 
@@ -15,5 +17,7 @@ app.use(fileUpload({
 //routes
 
 app.use(postRoutes)
+console.log(__dirname);
+app.use(express.static(join(__dirname, '../client/build')))
 
 export default app
