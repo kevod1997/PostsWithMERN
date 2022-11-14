c9ba6052b5ae02f4e3049c35effea20581fb754f
Kevin Defalco <kevindefalco@gmail.com>
2022-11-14 14:09:22 -0300

    fix

--- server/db.js
+++ server/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "../config.js";
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-mongoose.connection.on("connected", () => {
-  console.log("Mongodb is connected to", mongoose.connection.db.databaseName);
-});
+export async function connectDb() {
+  try {
+    const db = await mongoose.connect(MONGODB_URI);
+    console.log('connected to', db.connection.name);
+  } catch (error) {
+    console.log(error);
+  }
+}
