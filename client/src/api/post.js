685adc70ce9244949f86ac2bdfdca7e35925d139
Kevin Defalco <kevindefalco@gmail.com>
2022-11-14 13:50:07 -0300

    fixing

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -2,18 +2,17 @@ import axios from "axios";
 
 export const getPostsRequest = async ()=>  await  axios.get('/posts')
 
-export const createPostRequest = async (post)=>  {
-    const form = new FormData()
-
-    for (let key in post){
-        form.append(key, post[key]) 
+export const createPostRequest = async (post) => {
+    const form = new FormData();
+    for (let key in post) {
+      form.append(key, post[key]);
     }
-   return await  axios.post('/posts', form,  {
-        headers: {
-            "Content-Type": "multipart/form-data"
-        }
-    })
-}
+    return await axios.post("/api/posts", form, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  };
 
 export const deletePostRequest = async (id)=>  await  axios.delete('/posts/' + id)
 
