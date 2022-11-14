02a41e529511f3b1df0c0c8a3020b08c52430f5d
Kevin Defalco <kevindefalco@gmail.com>
2022-11-14 16:00:43 -0300

    cahnge

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -15,11 +15,11 @@ function HomePage() {
       </div>
     );
     return (
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {posts.map((post) => (
-        <PostCard post={post} key={post._id}/>
+        <PostCard key={post._id} post={post} />
       ))}
-      </div>
+    </div>
     )
   }
 
