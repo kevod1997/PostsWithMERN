59e8bbd3d2f93b880e9a34938190cd65e37c8e54
Kevin Defalco <kevindefalco@gmail.com>
2022-11-14 14:03:52 -0300

    fix

--- client/src/api/post.js
+++ client/src/api/post.js
@@ -1,21 +1,32 @@
 import axios from "axios";
 
-export const getPostsRequest = async ()=>  await  axios.get('/posts')
+export const getPostsRequest = async () => await axios.get("/api/posts");
+
+export const getPostRequest = async (id) => await axios.get("/api/posts/" + id);
+
+export const deletePostRequest = async (id) =>
+  await axios.delete("/api/posts/" + id);
 
 export const createPostRequest = async (post) => {
-    const form = new FormData();
-    for (let key in post) {
-      form.append(key, post[key]);
-    }
-    return await axios.post("/api/posts", form, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-  };
+  const form = new FormData();
+  for (let key in post) {
+    form.append(key, post[key]);
+  }
+  return await axios.post("/api/posts", form, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
-export const deletePostRequest = async (id)=>  await  axios.delete('/posts/' + id)
-
-export const getPostRequest = async (id)=>  await  axios.get("/posts/" + id)
-
-export const updatePostRequest = async (id, newFields)=>  await  axios.put(`/posts/${id}`, newFields)
+export const updatePostRequest = async (id, newPostFields) => {
+  const form = new FormData();
+  for (let key in newPostFields) {
+    form.append(key, newPostFields[key]);
+  }
+  return axios.put("/api/posts/" + id, form, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
